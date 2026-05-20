206f2e6d017e6c85e40876a5cd6cce98e2a8a5e0
andrustore1-ai <andrustore1@gmail.com>
2026-05-20 05:02:35 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// ملف توافق لمن كان يستدعي firebase-config.js سابقاً
+window.OSKAR_FIREBASE_CONFIG_READY = true;
+window.OSKAR_FIREBASE_LOGIN_KEY = "cashier_units_pro_momen_628a4_885766842";
+window.OSKAR_FIREBASE_ROOT = "cashier_units_pro_momen_628a4_885766842";
+window.OSKAR_FIREBASE_FALLBACK_ROOTS = ["cashier_units_pro_momen_628a4_885766842"];
+window.OSKAR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDZM55q57Mwgf3t0WbzRYLnt2EGrY7lJMA",
+  authDomain: "ziada-8d6ed.firebaseapp.com",
+  databaseURL: "https://ziada-8d6ed-default-rtdb.firebaseio.com",
+  projectId: "ziada-8d6ed",
+  storageBucket: "ziada-8d6ed.firebasestorage.app",
+  messagingSenderId: "339410523312",
+  appId: "1:339410523312:web:ed341f1b5736dbb6dcd775",
+  measurementId: "G-54YVMQQRJ8"
+};
+window.OSKAR_FIREBASE_APP_SETTINGS = {
+  firebaseConfig: window.OSKAR_FIREBASE_CONFIG,
+  firebaseRoot: window.OSKAR_FIREBASE_ROOT,
+  fallbackRoots: window.OSKAR_FIREBASE_FALLBACK_ROOTS
+};
